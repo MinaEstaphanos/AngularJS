14e67d01d2c8029fc21294b0f803b064153f761e
mmilad <mina.hanna@gmail.com>
2017-07-22 16:51:07 +0200

    New folder

---
+++ module1-solution/scripts/lunchCheck.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', FoodController)
+
+    FoodController.$inject = ['$scope','$filter'];
+    function FoodController($scope, $filter) {
+        $scope.items = "";
+        $scope.parseInput = function () {
+            
+            var foods = $scope.items;
+            foods = foods.split(',');
+            console.log(foods);
+            $scope.msg = "Too Much!";
+
+        };
+
+
+    }
+})();
