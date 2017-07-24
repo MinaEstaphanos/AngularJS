0c1ff671ccde88784b08672b06a5e123354f1f7a
mmilad <mina.hanna@gmail.com>
2017-07-25 00:42:51 +0200

    New folder

---
+++ module2-solution/scripts/app.js
@@ -0,0 +1,66 @@
+﻿(function () {
+    'use strict';
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', checkLunch)
+
+    checkLunch.$inject = ['$scope'];
+    function checkLunch($scope) {
+        $scope.lunchItems = "";
+        $scope.inputState = "";
+        $scope.msgState = "";
+
+
+        $scope.performCheck = function () {
+
+            var lunchItems = parseInput();
+            var numberOfItems = countItems(lunchItems);
+            console.log(numberOfItems);
+
+            setAppropriateMessage(numberOfItems);
+            setAppropriateStyle(numberOfItems);
+            
+        };
+        
+        function parseInput(input) {
+            return $scope.lunchItems.split(',');
+        }
+        function countItems(items) {
+            var itemCount = 0;
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].trim() != "")
+                    itemCount++;
+            }
+            return itemCount;
+        }
+        function setAppropriateMessage(numberOfItems) {
+
+            if (0 == numberOfItems)
+                $scope.msg = "Please enter data first";
+            else if (numberOfItems > 3)
+                $scope.msg = "Too Much!";
+            else
+                $scope.msg = "Enjoy!";
+        }
+        function setAppropriateStyle(numberOfItems) {
+            console.log(numberOfItems);
+            if (0 == numberOfItems) {
+                $scope.inputState = "inputError";
+                $scope.msgState = "msgError";
+                return;
+            }
+            else if (0 < numberOfItems && 3 >= numberOfItems) {
+                $scope.inputState = "inputSuccess";
+                $scope.msgState = "msgSuccess";
+                return;
+            }
+            $scope.inputState = "inputSuccess";
+            $scope.msgState = "msgError";
+
+        }
+
+
+
+
+
+    }
+})();
